f55bb3280dc6c59524c84891fb77bf65152e9a8b
letoya-ui <letoya@caughmanmason.com>
2026-02-28 00:22:34 -0500

    Update auth.py

--- LoanMVP/routes/auth.py
+++ LoanMVP/routes/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, flash, request, current_app,  get_flashed_messages
+from flask import Blueprint, render_template, redirect, url_for, flash, request, current_app,  get_flashed_messages, session
 from flask_login import login_user, logout_user, login_required, current_user
 from LoanMVP.models.user_model import User
 from LoanMVP.extensions import db
