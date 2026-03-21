ca65aedf6fd187dc8d8dbb6c751153571d5f814b
letoya-ui <letoya@caughmanmason.com>
2026-03-21 13:44:38 -0400

    Update tracking.py

--- LoanMVP/routes/tracking.py
+++ LoanMVP/routes/tracking.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, Response, request
 from LoanMVP.extensions import db
 from LoanMVP.models.loan_models import DocumentEvent
+from LoanMVP.models.partner_models import PartnerInviteEvent
 
 tracking_bp = Blueprint("tracking", __name__, url_prefix="/track")
+
 
 @tracking_bp.route("/pixel")
 def pixel():
@@ -10,7 +12,6 @@ def pixel():
     borrower_id = request.args.get("borrower_id")
     doc_name = request.args.get("doc", "Pre-Approval Letter")
 
-    # Save event
     event = DocumentEvent(
         loan_id=loan_id,
         borrower_id=borrower_id,
@@ -23,7 +24,33 @@ def pixel():
     db.session.add(event)
     db.session.commit()
 
-    # Return 1x1 transparent gif pixel
+    gif = (
+        b"GIF89a\x01\x00\x01\x00\xf0\x00\x00\x00\x00\x00"
+        b"\x00\x00\x00!\xf9\x04\x01\x00\x00\x00\x00,\x00"
+        b"\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02D\x01\x00;"
+    )
+
+    return Response(gif, mimetype="image/gif")
+
+
+@tracking_bp.route("/partner-pixel")
+def partner_pixel():
+    partner_id = request.args.get("partner_id")
+    token = request.args.get("token")
+    request_id = request.args.get("request_id")
+
+    event = PartnerInviteEvent(
+        partner_id=partner_id,
+        invite_token=token,
+        request_id=request_id,
+        event_type="opened",
+        user_agent=request.headers.get("User-Agent"),
+        ip_address=request.remote_addr
+    )
+
+    db.session.add(event)
+    db.session.commit()
+
     gif = (
         b"GIF89a\x01\x00\x01\x00\xf0\x00\x00\x00\x00\x00"
         b"\x00\x00\x00!\xf9\x04\x01\x00\x00\x00\x00,\x00"
